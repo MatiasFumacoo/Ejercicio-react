93724cffdbcda6d9d0055ed84eedf595f3aafc8a
MatiasFumacoo <MatiasFumacoo@gmail.com>
2021-08-02 13:24:37 -0300

    Archivos faltantes
    Se suben los archivos que faltan del primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import UserDetails from './Components/UserDetails/UserDetails';
 import Users from './Components/Users/Users';
 
 function App() {
   return (
-    <BrowserRouter>
-
+    <div className="App">
       <header className="App-header">
         <p>
           Mi proyecto
         </p>
       </header>
-
-      <Switch>
-        <Route path="/" component={Users} exact />
-        <Route path="/UserDetails" component={UserDetails} exact />
-      </Switch>
-
-    </BrowserRouter>
+      <div>
+          <Users></Users>
+      </div>
+    </div>
   );
 }
 
