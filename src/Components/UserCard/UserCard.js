9ce1881525faf144a6dfa2c04c67b3fcacf45e92
MatiasFumacoo <MatiasFumacoo@gmail.com>
2021-08-03 01:56:30 -0300

    Update UserCard.js

--- src/Components/UserCard/UserCard.js
+++ src/Components/UserCard/UserCard.js
@@ -12,13 +12,25 @@ function UserCard(props) {
     }
 
     return (
-        
-        user && <div className="feature">
-            <img src={user?.picture.thumbnail} onClick={() => routeChange(user) }></img>
-            <p>{user.name.title} {user.name.first} {user.name.last}</p>
-            <p>Country: {user.location.country} </p>
-            <p>City: {user.location.city} </p>
-        </div>             
+        user && <div className="card-box">
+            <div className={'card feature border-' + user.gender}>    
+                <div className="face">
+                    <div className="information-text">
+                        <p>{user.name.title} {user.name.first} {user.name.last}</p>
+                    </div>
+                    <p>Hover the mouse to flip for more information.</p>
+                </div>
+                <div className="face back">
+                    <img className="user-image" src={user?.picture.thumbnail} onClick={() => routeChange(user) }></img>
+                    <h5>Click the image for details.</h5>
+                    <div className="information-text">
+                        <p>Country: {user.location.country} </p>
+                        <p>City: {user.location.city} </p>
+                        <p>Street: {user.location.street.name} {user.location.street.number}</p>
+                    </div>
+                </div>    
+            </div>
+        </div>
     );
 }
 
