b50ca61cf8191820ba2553721c6f66414c7d875c
MatiasFumacoo <MatiasFumacoo@gmail.com>
2021-08-03 01:51:33 -0300

    update view userCard
    add more function in userCard, flip in hover, colours and more styles

--- src/Components/UserDetails/UserDetails.js
+++ src/Components/UserDetails/UserDetails.js
@@ -7,11 +7,10 @@ function UserDetails(props) {
 
     useEffect(() => {
         setUser(props.location.state);
-        console.log(user);
     }, [])
       
     return (
-        <div className="card">
+        <div className="card-details">
             <img className="imagen" src={user?.picture.large}></img>
             <h3>UserName: {user?.login.username}</h3>
             <h3>First Name: {user?.name.first}</h3>
